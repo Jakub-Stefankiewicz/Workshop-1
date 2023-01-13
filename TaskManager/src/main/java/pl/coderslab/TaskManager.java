c3e70e1fa94fc1e33295084bb36e45a2756dd72d
pannorris <jakub.stefankiewicz@wp.pl>
2023-01-13 21:00:01 +0100

    dodanie wczytywania i wyświetlania pliku

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,12 +1,23 @@
 package pl.coderslab;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TaskManager {
     public static void main(String[] args) {
-//        File zadania=new File("tasks.csv");
-//        Scanner scan = new Scanner(zadania);
+        StringBuilder reading = new StringBuilder();
+        try {
+            File file = new File("tasks.csv");
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                reading.append(scan.nextLine() + "\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku tasks.");
+        }
+        System.out.println(reading.toString());
 
         }
 }
