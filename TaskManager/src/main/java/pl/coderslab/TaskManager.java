fddd401eab5bcca352f0321058ab3e39d519ebad
pannorris <jakub.stefankiewicz@wp.pl>
2023-01-13 21:50:35 +0100

    pierwsza metoda- lista

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -7,17 +7,37 @@ import java.util.Scanner;
 
 public class TaskManager {
     public static void main(String[] args) {
-        StringBuilder reading = new StringBuilder();
-        try {
-            File file = new File("tasks.csv");
-            Scanner scan = new Scanner(file);
-            while (scan.hasNextLine()) {
-                reading.append(scan.nextLine() + "\n");
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Brak pliku tasks.");
-        }
-        System.out.println(reading.toString());
+        System.out.println(ConsoleColors.BLUE + "Please select an option" + ConsoleColors.RESET);
+        optionsList();
 
+        String option;
+        String [][] list= new String[4][4];
+        Scanner scan= new Scanner(System.in);
+
+        option=scan.nextLine();
+        if (option.contentEquals("list")) {
+            StringBuilder reading = new StringBuilder();
+            try {
+                File file = new File("tasks.csv");
+                Scanner tasks = new Scanner(file);
+                while (tasks.hasNextLine()) {
+                    reading.append(tasks.nextLine() + "\n");
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Brak pliku tasks.");
+            }
+            System.out.println(reading.toString());
+        } else {
+            System.out.println("coś nie tak");
+        }
+
+        }
+
+        public static void optionsList(){
+            String optionsList[]={"add", "remove", "list", "exit"};
+            for (int i = 0; i < optionsList.length; i++) {
+                System.out.println(optionsList[i]);
+            }
+            return;
         }
 }
