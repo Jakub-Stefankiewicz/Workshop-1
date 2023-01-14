6c75dd9d8e7d13737d991661de4440acc25d1ff0
pannorris <jakub.stefankiewicz@wp.pl>
2023-01-14 11:46:16 +0100

    tablica tasks wypełniona plikiem csv

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,7 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,57 +13,111 @@ import java.util.Scanner;
 
 public class TaskManager {
     public static void main(String[] args) {
+
+    /* lista dostępnych do wyboru opcji: **/
         System.out.println(ConsoleColors.BLUE + "Please select an option" + ConsoleColors.RESET);
         optionsList();
 
+    /* wybór opcji: **/
         String option;
         Scanner scan = new Scanner(System.in);
         option = scan.nextLine();
-//        switch (option){
-//            case "list":
-//                tasksList();
-//                break;
-//            default:
-//                System.out.println("wybierz dobrze");
-//        }
+        switch (option){
+            case "list":
+                tasksList();
+                break;
+            default:
+                System.out.println("wybierz dobrze");
+        }
 
     }
 
+    /* Użyte metody **/
+    /* metoda z listą opcji do wybrou:**/
     public static void optionsList() {
         String optionsList[] = {"add", "remove", "list", "exit"};
         for (int i = 0; i < optionsList.length; i++) {
             System.out.println(optionsList[i]);
         }
-        return;
     }
+
 
     public static void tasksList() {
         StringBuilder reading = new StringBuilder();
         try {
             File file = new File("tasks.csv");
-            Scanner tasks = new Scanner(file);
+            Scanner tasksFromFile = new Scanner(file);
             long lines = 0;
             Path path = Paths.get("tasks.csv");
-            try {
-                lines = Files.lines(path).count();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println(lines);
-            int linesInt = (int) lines;
-            String[][] list = new String[3][linesInt];
 
-            while (tasks.hasNextLine()) {
-                reading.append(tasks.nextLine() + "\n");
+            /* pobieranie liczby wierszy:**/
+                try {
+                    lines = Files.lines(path).count();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(lines);
+                int linesInt = (int) lines;
+
+            /* definiowanie wielkości tablicy:**/
+            tasks=new String[linesInt][3];
+
+            for (int i = 0; i < linesInt; i++) {
+                for (int j = 0; j < 3; j++) {
+                    tasks[i][j]="test";
+                }
             }
+
+            for (int i = 0; i < linesInt; i++) {
+
+            }
+
+
+            for (int i = 0; i < linesInt; i++) {
+                StringBuilder wiersz= new StringBuilder();
+                for (int j = 0; j < 3; j++) {
+                    wiersz.append(tasks[i][j] + ",");
+                }
+                System.out.println(wiersz);
+            }
+
+
+            int counter=0;
+            String[] liniaStr= new String[linesInt];
+            while (tasksFromFile.hasNextLine()) {
+                String linia=tasksFromFile.nextLine();
+                liniaStr[counter]=linia;
+                System.out.println(counter);
+                System.out.println(liniaStr[counter]);
+                String[] str= liniaStr[counter].split(",");
+                for (int i = 0; i < 3; i++) {
+                    tasks[counter][i]=str[i];
+                }
+                counter++;
+                }
+            for (int i = 0; i < tasks.length; i++) {
+                System.out.println("linia numer" + i);
+                for (int j = 0; j < 3; j++) {
+                    System.out.println("kolumna numer" + j);
+                    System.out.println(tasks[i][j]);
+                }
+
+            }
+
+//            while (tasksFromFile.hasNextLine()) {
+//                reading.append(tasksFromFile.nextLine() + "\n");
+//                String[] str= reading.toString().split(",");
+//                for (int i = 0; i < str.length; i++) {
+//                    System.out.println(str[i]);
+//                }
+//            }
         } catch (FileNotFoundException e) {
             System.out.println("Brak pliku tasks.");
         }
         System.out.println(reading.toString());
-        return;
-
-//        static String[][] tasks;  ????????????????
 
     }
+
+    static String[][] tasks;
 }
 
